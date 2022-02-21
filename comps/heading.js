6220b26d0c47518d61e72c80a897bf58b1585f72
SeanHLama <slam152@my.bcit.ca>
2022-02-21 14:42:15 -0800

    added heading comp

---
+++ comps/heading.js
@@ -0,0 +1,44 @@
+//MUST HAVE - CREATE A TEMPLATE TAG
+var template_heading = document.createElement("template"); //<template> </template> RULE
+
+//To-do - CREATE THE UI HERE!
+template_heading.innerHTML = `
+
+<style>
+#heading{
+    font-size: 26px;
+    color: #382D72;
+    margin: 2em;    
+}
+
+
+</style>
+
+<div id="heading">poops</div>
+`;
+
+//MUST HAVE - CREATE A CLASS WITH HTMLELEMENT POWERS (interfaces/functionalities)
+class TheHeading extends HTMLElement {
+
+    //MUST HAVE - CREATE A CONSTRUCTOR TO DO INITAL ASSOCIATIONS
+    constructor(){
+        super(); //pass on the HTMLElement super powers!
+        this.attachShadow({mode:"open"}) //Attach it to the shadowRoot
+
+        //To-do - CREATE THE STATES FOR THE UI HERE!
+    }
+
+    //MUST HAVE - FUNCTION THAT RUNS AFTER IT'S CONNECTED
+    connectedCallback(){
+        this.shadowRoot.appendChild(template_heading.content.cloneNode(true)); //use the template to make a clone
+
+        if(this.getAttribute("head_text")){
+            this.shadowRoot.querySelector("#heading").innerText = this.getAttribute("head_text");
+        }
+    }
+
+    //To-do - CREATE THE FUNCTIONALITIES HERE!
+}
+
+//MUST HAVE - define the tag for the custom elements
+customElements.define("the-heading", TheHeading)
