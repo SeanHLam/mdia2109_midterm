e50921f1b61f5fdab88ad39969f354c13bb6f380
mmogk <mmogk@my.bcit>
2022-02-28 12:20:21 -0800

    components init

--- comps/speechBubble.js
+++ comps/speechBubble.js
@@ -3,25 +3,43 @@ var template_statitem = document.createElement("template"); //<template> </templ
 
 //To-do - CREATE THE UI HERE!
 template_statitem.innerHTML = `
+<style>
+  #speech {
+ 
+  }
+
+</style>
+
+<div id='speech'>
+  <img src='./imgs/speech1.svg' />
+</div>
 `;
 
 //MUST HAVE - CREATE A CLASS WITH HTMLELEMENT POWERS (interfaces/functionalities)
 class TheStatItem extends HTMLElement {
 
     //MUST HAVE - CREATE A CONSTRUCTOR TO DO INITAL ASSOCIATIONS
-    constructor(){
+    constructor() {
         super(); //pass on the HTMLElement super powers!
-        this.attachShadow({mode:"open"})
+        this.attachShadow({ mode: "open" })
 
         //To-do - CREATE THE STATES FOR THE UI HERE!
     }
 
     //MUST HAVE - FUNCTION THAT RUNS AFTER IT'S CONNECTED
-    connectedCallback(){
+    connectedCallback() {
         this.shadowRoot.appendChild(template_statitem.content.cloneNode(true)); //use the template to make a clone
+        
+        }
     }
 
     //To-do - CREATE THE FUNCTIONALITIES HERE!
+    showSpeech() {
+        document.getElementById('speech').style.display = "block";
+     }
+
+
+    
 }
 
 //MUST HAVE - define the tag for the custom elements
